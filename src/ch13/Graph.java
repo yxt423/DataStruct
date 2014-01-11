fc483ee7f0d3a56e03ad9bd6bda7916cf2cc0be9
Xintong Yu <yxt423@gmail.com>
2014-01-11 01:21:08 -0500

    ch13 bfs

--- src/ch13/Graph.java
+++ src/ch13/Graph.java
@@ -17,6 +17,7 @@ public class Graph {
 	private int adjMat[][];      // adjacency matrix
 	private int nVerts;          // current number of vertices
 	private StackX theStack;     // used in dfs
+	private Queue theQueue;      // used in bfs
 	
     public Graph(){              // constructor
       vertexList = new Vertex[MAX_VERTS];
@@ -27,6 +28,7 @@ public class Graph {
          for(int k=0; k<MAX_VERTS; k++)   //    matrix to 0
             adjMat[j][k] = 0;
       theStack = new StackX();
+      theQueue = new Queue();
     }  // end constructor
     
     public void dfs(){  // depth-first search
@@ -59,6 +61,26 @@ public class Graph {
     	return -1;
     }
     
+    public void bfs(){                   // breadth-first search
+    	vertexList[0].wasVisited = true;
+    	displayVertex(0);
+    	theQueue.insert(0);
+    	int v2;
+    	
+    	while(!theQueue.isEmpty()){
+    		int v1 = theQueue.remove();
+    		for(int i=0; i<nVerts; i++){
+    			if(adjMat[v1][i] == 1 && vertexList[i].wasVisited == false){
+    				vertexList[i].wasVisited = true;
+    				displayVertex(i);
+    				theQueue.insert(i);
+    			}
+    		}
+    	}
+    	
+    	for(int j=0; j<nVerts; j++)
+    		vertexList[j].wasVisited = false;
+    }
     public void addVertex(char lab){
     	vertexList[nVerts++] = new Vertex(lab);
     }
--- src/ch13/MainApp.java
+++ src/ch13/MainApp.java
@@ -2,9 +2,26 @@ package ch13;
 
 public class MainApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args){
+	    Graph theGraph = new Graph();
+	    theGraph.addVertex('A');    // 0  (start for bfs)
+	    theGraph.addVertex('B');    // 1
+	    theGraph.addVertex('C');    // 2
+	    theGraph.addVertex('D');    // 3
+	    theGraph.addVertex('E');    // 4
+	
+	    theGraph.addEdge(0, 1);     // AB
+	    theGraph.addEdge(1, 2);     // BC
+	    theGraph.addEdge(0, 3);     // AD
+	    theGraph.addEdge(3, 4);     // DE
+	
+	    System.out.print("depth-first visits: ");
+	    theGraph.dfs();             // depth-first search
+	    System.out.println();
+	    
+	      System.out.print("breadth-first visits: ");
+	      theGraph.bfs();             // breadth-first search
+	      System.out.println();
+    }  // end main()
 
 }
